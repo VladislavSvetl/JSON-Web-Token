968267073852e6a10b3e69542d758cbda3e01d50
svetl <v.svetlichniy1999@gmail.com>
2022-12-11 19:29:33 +0300

    Added Login function

--- authorization/users/views.py
+++ authorization/users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt, datetime
 
 
 class RegisterView(APIView):
@@ -13,3 +16,30 @@ class RegisterView(APIView):
 
         return Response(serializer.data)
 
+
+class LoginView(APIView):
+
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('Пользователь не существует')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=720),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt': token
+        }
+
+        return response
